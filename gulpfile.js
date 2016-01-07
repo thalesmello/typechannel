e47a575bb4713913afd815bca187db7cc5423955
Thales Mello <thalesmello@gmail.com>
2016-01-06 23:29:51 -0200

    initial commit setting up system

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var jasmine = require('gulp-jasmine');
+var merge = require('merge2');
+var notify = require('gulp-notify');
+
+var buildProject = ts.createProject("tsconfig.json");
+
+gulp.task('build', () => {
+    var tsStream = gulp.src('src/**/*.ts')
+        .pipe(ts(buildProject));
+
+    return merge([
+        tsStream.dts.pipe(gulp.dest('bin/src')),
+        tsStream.js.pipe(gulp.dest('bin/src'))
+    ]);
+});
+
+gulp.task('test', ['build', 'build_test'], () => {
+    return gulp.src('bin/spec/*_spec.js')
+        .pipe(jasmine())
+        .on("error", notify.onError("Error: <%= error.message %>"))
+        .pipe(notify('done'));
+});
+
+var specProject = ts.createProject(tsOpts);
+
+gulp.task('build_test', () => {
+    return gulp.src('spec/**/*_spec.ts')
+        .pipe(ts(specProject))
+        .pipe(gulp.dest('bin/spec'));
+})
+
+gulp.task('watch', ['test'], () => {
+    gulp.watch(['spec/**/*_spec.ts', 'src/**/*.ts'], ['test']);
+});
